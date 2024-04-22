3c724d8caa62f311d9700f251951ddd1d2f1ab00
CLIUKELLY <chengyanliudp@gmail.com>
2024-04-22 11:39:24 -0400

    Update FrontEnd.py

--- FrontEnd.py
+++ FrontEnd.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def display_data_catalog():
     # Read the data catalog file
-    data_catalog = pd.read_csv('/Users/kellyliu/Documents/GitHub/Customer-Personality-Analysis-2.0/Data/Preprocessed Data/Final Preprocessed Data.csv')
+    data_catalog = pd.read_csv('https://raw.githubusercontent.com/McGill-MMA-EnterpriseAnalytics/Customer-Personality-Analysis-2.0/main/Data/Preprocessed%20Data/Final%20Preprocessed%20Data.csv?token=GHSAT0AAAAAACL3VCJB4QQSO53AD2M7BBPIZRGSKYA')
 
     # Display the data catalog in a table
     st.table(data_catalog)
